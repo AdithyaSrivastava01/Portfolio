8c0516cf67f40d0b975ee21e70113b4ff16d51b0
AdithyaSrivastava01 <adithyasrivastava01@gmail.com>
2023-10-11 19:03:41 +0530

    Proj_done

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,6 +96,44 @@ const scrollActive = () =>{
 }
 window.addEventListener('scroll',scrollActive)
 /*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme= 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = ()=> document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = ()=> themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon=== 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+
+}
+
+themeButton.addEventListener('click',()=>{
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.home__profile, .about__image, .contact__mail`,{origin:'right'})
+
+sr.reveal(`.home__name, .home__info, 
+           .about__container .section__title-1, .about__info,
+           .contact__data`,{origin:'left'})
+
+sr.reveal(`.projects__card`,{interval:100})
